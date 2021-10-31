28b35226004c61892d110bea84268e153f262b9e
Okan Sen <okansencma@gmail.com>
2021-10-31 20:40:20 +0300

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,32 @@
+let allMusic = [
+    {
+        name: "Okan Sen - Divinity",
+        artist: "Okan Sen",
+        img: "https://drive.google.com/uc?export=view&id=1FBpOfkhF-04ziwrI0Zz8IZ3eGkfLbfVu",
+        src: "https://drive.google.com/uc?export=view&id=1dA-PgDMTHdfy3vReQLa9r0Ou0IuE3yLd",
+    },
+    {
+        name: "Okan Sen - Genesis of Eradication",
+        artist: "Okan Sen",
+        img: "https://drive.google.com/uc?export=view&id=1AlS4VqiC9WtgWzw73Tnu4QF05_fqk8uc",
+        src: "https://drive.google.com/uc?export=view&id=1IROlWctNUrb5PQud2xfUAIGrpyKRA3tp",
+    },
+    {
+        name: "Okan Sen - Tone of Love",
+        artist: "Okan Sen",
+        img: "https://drive.google.com/uc?export=view&id=1Z5FEND8uLGzhRhkCsYANSCTXmiOT7kwN",
+        src: "https://drive.google.com/uc?export=view&id=1bWvq12G9YCM_DvSFdF0gWKwlPj5z6xku",
+    },
+    {
+        name: "Okan Sen - Not to be Overwhelmed",
+        artist: "Okan Sen",
+        img: "https://drive.google.com/uc?export=view&id=1hxIdSGfGXBicK5B3maddz5hGLXGzNDqa",
+        src: "https://drive.google.com/uc?export=view&id=1acAbFO7S3S0-dd9dlVjQtT5Zww1viVMf",
+    },
+    {
+        name: "Okan Sen - Raging Dogs",
+        artist: "Okan Sen",
+        img: "https://drive.google.com/uc?export=view&id=15UgmSd5H2ZqyktYxZiU3aHVHtJMyqNCe",
+        src: "https://drive.google.com/uc?export=view&id=1fgxD-FEto2iyzQaqg7EwXUeFNNPCFEwB",
+    },
+];
